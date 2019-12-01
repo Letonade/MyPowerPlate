e760f7510106e351c29091ee62e3b9ff3f546943
LetonadeMainProfile <cocoscorpio94@hotmail.fr>
2019-12-01 18:37:47 +0100

    Last

--- Last01-12-2019.sql
+++ Last01-12-2019.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  Dim 01 déc. 2019 à 11:29
+-- Généré le :  Dim 01 déc. 2019 à 18:35
 -- Version du serveur :  10.4.8-MariaDB
 -- Version de PHP :  7.3.11
 
@@ -65,7 +65,8 @@ CREATE TABLE `compte` (
 --
 
 INSERT INTO `compte` (`idCompte`, `login`, `password`, `profil`) VALUES
-(1, 'admin', 'admin', 'Manager');
+(1, 'admin', '$2y$10$sr0.gq1ds2TdMgqi1UzwIu1wxejp2J1ysA2nH14fgpq10QK9WiX2m', 'Manager'),
+(2, 'admin2', '$2y$10$URg9x7N80ffkGKBXPJodluw0GDX8vfF1IVUkzOqp9cCkwZiqJNp8S', 'Manager');
 
 -- --------------------------------------------------------
 
@@ -82,16 +83,21 @@ CREATE TABLE `produit` (
   `prixUnitaire` int(11) NOT NULL DEFAULT 0,
   `tva` int(11) NOT NULL DEFAULT 0,
   `description` text DEFAULT NULL,
-  `reference` varchar(220) NOT NULL
+  `reference` varchar(220) NOT NULL,
+  `lienImage` varchar(220) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `produit`
 --
 
-INSERT INTO `produit` (`idProduit`, `libelle`, `categorie`, `marque`, `quantite`, `prixUnitaire`, `tva`, `description`, `reference`) VALUES
-(2, 'Rogue V2700', 'Carte maman', 'Asus', 28, 312, 12, 'Une carte maman de performance pas du tous renomé', 'A-R-V700'),
-(3, 'Gamer+ 17.5\"', 'Ecran', 'iiyama', 52, 112, 12, 'Un écran de bonne fabrique, flemme..', 'I-G+-17.5\"');
+INSERT INTO `produit` (`idProduit`, `libelle`, `categorie`, `marque`, `quantite`, `prixUnitaire`, `tva`, `description`, `reference`, `lienImage`) VALUES
+(2, 'Rogue V2700', 'Carte maman', 'Asus', 28, 312, 12, 'Une carte maman de performance pas du tous renomé', 'A-R-V700', 'https://media.hardware.fr/r500/ld/products/00/04/94/74/LD0004947482_2.jpg'),
+(3, 'Gamer+ 27\"', 'Ecran', 'iiyama', 52, 112, 12, 'Un écran de bonne fabrique, flemme..', 'I-G+-27\"', 'https://media.ldlc.com/r1600/ld/products/00/03/67/57/LD0003675708_2.jpg'),
+(4, 'Rogue V2700', 'Carte maman', 'Asus', 28, 312, 12, 'Une carte maman de performance pas du tous renomé', 'test1', 'https://media.hardware.fr/r500/ld/products/00/04/94/74/LD0004947482_2.jpg'),
+(5, 'Rogue V2700', 'Carte maman', 'Asus', 28, 312, 12, 'Une carte maman de performance pas du tous renomé', 'test2', 'https://media.hardware.fr/r500/ld/products/00/04/94/74/LD0004947482_2.jpg'),
+(6, 'Rogue V2700', 'Carte maman', 'Asus', 28, 312, 12, 'Une carte maman de performance pas du tous renomé', 'test3', 'https://media.hardware.fr/r500/ld/products/00/04/94/74/LD0004947482_2.jpg'),
+(7, 'Rogue V2700', 'Carte maman', 'Asus', 28, 312, 12, 'Une carte maman de performance pas du tous renomé', 'test4', 'https://media.hardware.fr/r500/ld/products/00/04/94/74/LD0004947482_2.jpg');
 
 --
 -- Index pour les tables déchargées
@@ -113,7 +119,8 @@ ALTER TABLE `compte`
 -- Index pour la table `produit`
 --
 ALTER TABLE `produit`
-  ADD PRIMARY KEY (`idProduit`);
+  ADD PRIMARY KEY (`idProduit`),
+  ADD UNIQUE KEY `reference` (`reference`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -129,13 +136,13 @@ ALTER TABLE `commande`
 -- AUTO_INCREMENT pour la table `compte`
 --
 ALTER TABLE `compte`
-  MODIFY `idCompte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idCompte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `produit`
 --
 ALTER TABLE `produit`
-  MODIFY `idProduit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idProduit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- bb/Recursif/head.php
+++
@@ -1,4 +0,0 @@
-        <title>eCOM</title>
-        <meta charset="UTF-8">
-        <meta name="Author" content="Alexandre Lê">
-        <link rel="stylesheet" href="./Assets/bootstrap-4.4.1-dist/css/bootstrap.css">
